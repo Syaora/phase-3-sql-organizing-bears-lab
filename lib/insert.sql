5b5d331becfe5d54374fb844ccc286308e1e010e
Elizabeth Nguyen <saper209@yahoo.com>
2022-07-31 05:21:50 -0700

    commit tree 226bce50b7009f64d3aa05ea9617204b003acf4a

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 30, 'M', 'brown', 'chill', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 10, 'M', 'brown', 'rowdy', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 40, 'F', 'black', 'chill', 0);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 20, 'M', 'brown', 'aloof', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 10, 'F', 'white', 'aloof', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 20, 'M', 'white', 'grumpy', 0);
+INSERT INTO bears VALUES (7, 'Wendy', 21, 'F', 'black', 'friendly', 1);
+INSERT INTO bears VALUES (8, null, 30, 'M', 'brown', 'chill', 1);
